48f12735ca4f7879e975c372bf8810b364687a31
Lomeli12 <anthonylomeli11@gmail.com>
2016-08-01 22:19:45 -0700

    Stupidly used Client I18n.

--- src/main/kotlin/net/lomeli/mcslack/core/helper/LangHelper.kt
+++ src/main/kotlin/net/lomeli/mcslack/core/helper/LangHelper.kt
@@ -1,15 +1,17 @@
 package net.lomeli.mcslack.core.helper
 
 import com.google.common.base.Strings
-import net.minecraft.client.resources.I18n
+import net.minecraft.util.text.translation.I18n
 
 object LangHelper {
 
-    fun canLocalize(key: String) : Boolean = !Strings.isNullOrEmpty(key) && I18n.hasKey(key)
+    fun canLocalize(key: String) : Boolean = !Strings.isNullOrEmpty(key) && I18n.canTranslate(key)
 
     fun translate(key: String, vararg args: Any): String {
-        if (canLocalize(key))
-            return if (args != null && args.size > 0) I18n.format(key, *args) else I18n.format(key)
+        if (canLocalize(key)) {
+            val local = I18n.translateToLocal(key)
+            return if (args != null && args.size > 0) String.format(local, *args) else local
+        }
         return key
     }
 }
